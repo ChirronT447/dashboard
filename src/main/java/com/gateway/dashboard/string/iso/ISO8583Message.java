9c3e13e3a6bcdc3dcf32bd4b6bfc2b59029befcb
dgriffit <dgriffit@tcd.ie>
2024-03-18 00:46:24 +0000

    Leetcode questions

--- src/main/java/com/gateway/dashboard/string/iso/ISO8583Message.java
+++ src/main/java/com/gateway/dashboard/string/iso/ISO8583Message.java
@@ -2,15 +2,23 @@ package com.gateway.dashboard.string.iso;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public record ISO8583Message(String messageType, Map<String, String> fields) {
 
-    public static ISO8583Message from(final String message) {
-        final List<String> fieldsChunks = splitFields(message.substring(4));
-        final Map<String, String> fieldsMap = parseFields(fieldsChunks);
-        return new ISO8583Message(message.substring(0, 4), fieldsMap);
+    public static Optional<ISO8583Message> from(final String message) {
+        System.out.println("Received " + message);
+        if (message != null && !message.isBlank() && message.startsWith("IMT")) {
+            final List<String> fieldsChunks = splitFields(message.substring(4));
+            final Map<String, String> fieldsMap = parseFields(fieldsChunks);
+            return Optional.of(
+                    new ISO8583Message(message.substring(0, 4), fieldsMap)
+            );
+        } else {
+            return Optional.empty();
+        }
     }
 
     private static List<String> splitFields(final String fieldsData) {
